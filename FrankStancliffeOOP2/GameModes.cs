1573fc88f65f8ad6f95c4e940da7d380eb78ffef
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-04-01 20:32:18 +0100

    added additional classes

---
+++ FrankStancliffeOOP2/GameModes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrankStancliffeOOP2
+{
+    internal class GameModes
+    {
+        // 5 x dice
+        //Rules:
+        //Roll all 5 dice hoping for a 3-of-a-kind or better.
+        //	If 2-of-a-kind is rolled, player may choose to rethrow all, or the remaining dice.
+        //	3-of-a-kind: 3 points
+        //	4-of-a-kind: 6 points
+        //	5-of-a-kind: 12 points
+        //    First to a total of 20.
+        public void ThreeOrMore() { }
+
+        //2 x dice
+        //Rules:
+        //Roll the two dice, noting the total rolled each time.
+        //	If it is a 7 - stop.
+        //	If it is any other number - add it to your total.
+        //		If it is a double - add double the total to your score (3,3 would add 12 to your total)
+        public void SevensOut() { }
+    }
+}
---
+++ FrankStancliffeOOP2/Roller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrankStancliffeOOP2
+{
+    internal class Roller
+    {
+        public int[] Roll5Die()
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            int[] arrayOfDieRolls = { (die1.Roll()), (die2.Roll()) };
+            return arrayOfDieRolls;
+        }
+        public int[] Roll2Die() {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            Die die3 = new Die();
+            Die die4 = new Die();
+            Die die5 = new Die();
+            int[] arrayOfDieRolls = { (die1.Roll()), (die2.Roll()), (die3.Roll()) , (die4.Roll()) , (die5.Roll()) };
+            return arrayOfDieRolls;
+        }
+    }
+}
