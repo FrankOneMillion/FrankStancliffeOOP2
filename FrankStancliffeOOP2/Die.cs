b381c8bad8562961bfaebcbb3ef83ce66e93857a
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-04-01 17:05:35 +0100

    Add project files.

---
+++ FrankStancliffeOOP2/Die.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrankStancliffeOOP2
+{
+    internal class Die
+    {
+        static Random random = new Random();
+        private int _rollValue;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Roll()
+        {
+            _rollValue = random.Next(1,7);
+            return _rollValue;
+        }
+    }
+}
